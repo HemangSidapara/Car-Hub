becf21360718253ead79bad7a2ac330b72dc7bba
Hemang Sidapara <61095579+HemangSidapara@users.noreply.github.com>
2022-01-03 16:34:21 +0530

    Add files via upload

---
+++ carhub.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car Hub</title>
+    <link rel="stylesheet" href="css/carhub.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.1.2/dist/ionicons/ionicons.esm.js"></script>
+    <script src="https://kit.fontawesome.com/76603175a9.js" crossorigin="anonymous"></script>
+</head>
+    <body>
+        <div class="main">
+            <div class="nm">Car Hub<sup style="font-size:50px; text-transform:lowercase;">2.o</sup></div>
+            <div class="itm">
+                <a class="a" href="pages\login.php">
+                    <ion-icon class="icn" src="css\svg\sign-in-alt-solid.svg"></ion-icon>
+                </a>
+                <a class="a" href="pages\signup.php">
+                    <ion-icon class="icn" name="person-add-sharp"></ion-icon>
+                </a>
+                <a class="a" href="aboutus.php">
+                    <ion-icon class="icn" name="people-sharp"></ion-icon>
+                </a>
+                <a class="a" href="https://github.com/HemangSidapara">
+                    <ion-icon class="icn" name="logo-github"></ion-icon>
+                </a>
+            </div>
+
+        </div>
+
+        <div class="footer">
+            &copy;2020 CarHub All Rights Reserved 
+        </div>
+    </body>
+</html>
